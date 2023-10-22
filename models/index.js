597c864c91606b8fa115f72a4743c41a893684b9
[Andrew Epermanis] <[aepermanis@gmail.com]>
2023-10-22 15:58:18 -0400

    added first attempt at routes for all models"

--- models/index.js
+++ models/index.js
@@ -9,7 +9,7 @@ Product.belongsTo(Category);
 // Categories have many Products
 Category.hasMany(Product)
 // Products belongToMany Tags (through ProductTag)
-Product.belongsToMany(Tag, {Through: ProductTag})
+Product.belongsToMany(Tag, {through: ProductTag})
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {through: ProductTag})
 
